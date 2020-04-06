06b187e0863802783c9b53828b9fec2df430ba8f
Kapil Bhagat <kapbhaga@publicisgroupe.net>
2020-04-07 01:39:11 +0530

    NG-HTTP: Implemneted HTTP assignment with Observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { HttpErrorHandler } from './http-error-handler.service';
+import { MessageService } from './message.service';
+import { ToDoFormComponent } from './components/to-do-form/to-do-form.component';
+import { ToDoComponent } from './components/to-do-form/to-do/to-do.component';
+import { ToDoListComponent } from './components/to-do-form/to-do-list/to-do-list.component';
+import {FormsModule} from '@angular/forms';
+import { ToDoService } from './services/to-do.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToDoFormComponent,
+    ToDoComponent,
+    ToDoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    HttpErrorHandler,
+    MessageService,
+    ToDoService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
